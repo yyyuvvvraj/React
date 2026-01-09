694bd064ad5b09ce8583684599024d792aa8f7e8
Yuvraj Rajni Sachin Deshmukh <yyyuvvvraj@Yuvrajs-MacBook-Air.local>
2026-01-09 13:08:11 +0530

    State Class

---
+++ REACT-STATE-CLASS/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Counter from "./Counter";
+
+function App() {
+  return (
+    <>
+      <h1>States in React</h1>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
---
+++ REACT-STATE-CLASS/src/Counter.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export default function Counter() {
+    let [stateVariable,setStateVariable] = useState(10);
+    let [count,setCount] = useState(0);
+
+    function incCount() {
+        setCount(count + 1);
+    }
+    return (
+    <div>
+      <h3>Count = {count}</h3>
+      <button onClick={incCount}>Increase Count</button>
+    </div>
+  );
+}
